d851aa4a1de6f604cada8980c0da28b4dc9a07f4
Nestor <nestnick0@gmail.com>
2023-11-07 21:50:52 -0300

    Placeholder

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(3007, ()=>{
     console.log('Servidor funcionando');
 });
 
-app.get('/home', (req,res)=>{
+app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
-});
-
-app.get('/registro', (req,res)=>{
-    res.sendFile(__dirname + '/views/registro.html');
 });
 
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
-})
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
